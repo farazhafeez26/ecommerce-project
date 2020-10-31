f01042be0ff4bb798e5cce031e06e5d83f65ba4a
farazhafeez26 <faraz.hafiz@gmail.com>
2020-10-31 11:06:50 +0100

    added buttons and compnts

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,30 +1,61 @@
 import React, { useState } from "react";
 import Price from "./Price";
 import Shipping from "./Shipping";
+import Size from "./Size";
+import styled from "styled-components";
 
 export default function Product(props) {
   // Props is data from the parent
   // State is owned by the component
   const [quantity, setQuantity] = useState(1); // Array Destructuring
   return (
-    <div className="display-items">
-      <h4 style={{ color: "white" }}> {props.name} </h4>
-      <img src={props.imgSrc} className="product-image"></img>
+    <div className="boxitem">
+      <div>
+        <h4 style={{ color: "white" }}> {props.name} </h4>
+      </div>
+      <div>
+        <img src={props.imgSrc} className="product-image"></img>
+      </div>
 
-      <button onClick={() => setQuantity(quantity + 1)}>Add to cart</button>
-      <button onClick={() => setQuantity(0)}>remove</button>
-      <button onClick={() => setQuantity(quantity - 1)}>remove just one</button>
+      <div>
+        <QuantityContainer>
+          <QtyButton onClick={() => setQuantity(quantity + 1)}>Add to cart</QtyButton>
 
-      <h3>{quantity}</h3>
+          <Quantity>{quantity}</Quantity>
+        </QuantityContainer>
 
-      <Price value={props.price}> </Price>
-      <Shipping value={props.shipping}></Shipping>
+        <InfoContainer>
+          <Price value={props.price}> </Price>
+          <Shipping value={props.shipping}></Shipping>
+          <Size value={props.size}></Size>
+        </InfoContainer>
+      </div>
     </div>
   );
 }
 
+const Quantity = styled.h3`
+  font-size: 1.5em;
+  text-align: center;
+  color: black;
+`;
+
+const QuantityContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const QtyButton = styled.button`
+  height: 20px;
+  border-radius: 3px;
+`;
 //questions
 //how can i include in multiple properties of a product in one JSX file. For example shipping and price in one js file ?
 // align items is not working
-// how do i label stuff?
+
 // How is spacing being allocated between the elements being allocated?
---
+++ src/Size.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+class Size extends React.Component {
+  render() {
+    const { value } = this.props;
+
+    return <h4>{value}</h4>;
+  }
+}
+
+export default Size;
